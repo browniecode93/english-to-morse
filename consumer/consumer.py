319fb45d3ac4dccc372b18abbf460c7217f171dc
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-09 16:46:35 +0330

    show message body

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://tfhyafsd:AfCkvL237tDy1wlKfJeOJdRSoyBnuJ9G@fish.rmq.cloudamqp.com/tfhyafsd')
 
@@ -10,8 +10,9 @@ channel.queue_declare(queue='consumer')
 
 
 def callback(ch, method, properties, body):
-    print('Recieved in consumer')
-    print(body)
+    print('Received in consumer')
+    data = json.loads(body)
+    print(data)
 
 
 channel.basic_consume(queue='consumer', on_message_callback=callback, auto_ack=True)
