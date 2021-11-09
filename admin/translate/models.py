2bb5d7de5b1a88a6c6ec21bf980e1c7783689355
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-09 13:24:04 +0330

    create model

--- admin/translate/models.py
+++ admin/translate/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    eng_msg = models.CharField(max_length=800)
+    morse_msg = models.CharField(max_length=800)
